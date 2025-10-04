538e1d338c374a368a546a9f327f619dd4ac38cc
DanGithub1234 <dphan1234@outlook.com>
2025-10-04 15:53:06 +1000

    Sydnie-event-creation

--- travel/models.py
+++ travel/models.py
@@ -23,21 +23,21 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Name: {self.name}"
 
-class Event(db.Model):
-    __tablename__ = 'Events'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    description = db.Column(db.String(200))
-    image = db.Column(db.String(400))
-    currency = db.Column(db.String(3))
-    genre = db.Column(db.String(50))
-    # ... Create the Comments db.relationship
-	# relation to call event.comments and comment.event
-    comments = db.relationship('Comment', backref='event')
+# class Event(db.Model):
+#     __tablename__ = 'Events'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80))
+#     description = db.Column(db.String(200))
+#     image = db.Column(db.String(400))
+#     currency = db.Column(db.String(3))
+#     genre = db.Column(db.String(50))
+#     # ... Create the Comments db.relationship
+# 	# relation to call event.comments and comment.event
+#     comments = db.relationship('Comment', backref='event')
 	
-    # string print method
-    def __repr__(self):
-        return f"Name: {self.name}"
+#     # string print method
+#     def __repr__(self):
+#         return f"Name: {self.name}"
     
 
 
@@ -82,8 +82,36 @@ class Comment(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now())
     # add the foreign key
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    Event_id = db.Column(db.Integer, db.ForeignKey('Events.id'))
+    Event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
     # string print method
     def __repr__(self):
         return f"Comment: {self.text}"
+    
+
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    currency = db.Column(db.String(3))
+
+    # new fields for event creation
+    location = db.Column(db.String(200))
+    event_date = db.Column(db.Date)
+    start_time = db.Column(db.Time)
+    end_time = db.Column(db.Time)
+    ticket_details = db.Column(db.String(1000))
+    tickets_available = db.Column(db.Integer)
+    ticket_price = db.Column(db.Float)
+    image2 = db.Column(db.String(400))
+    image3 = db.Column(db.String(400))
+
+    # relationship to comments
+    comments = db.relationship('Comment', backref='destination')
+
+    def __repr__(self):
+        return f"Name: {self.name}"
