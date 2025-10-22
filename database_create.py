fffea0c85078cea0371961c1c9f6f5317b52f090
sclauson1 <sofiaclauson@gmail.com>
2025-10-22 17:11:01 +1000

    Delete database_create.py

--- database_create.py
+++
@@ -1,9 +0,0 @@
-from travel import db, create_app
-from travel.models import db, Event, Booking
-from sqlalchemy import inspect
-
-app = create_app()
-ctx = app.app_context()
-ctx.push()
-db.create_all()
-quit()
